115158436a2b37451bbc22b00c94a9580d3774c9
Henry's <Henry's@HOMEPC>
2017-02-21 22:48:44 -0600

    Add project files.

---
+++ Bookstore.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Bookstore.Web.Startup))]
+namespace Bookstore.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
